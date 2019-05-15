962f699569876b32ea8dabf2b80209b0b3796ed0
Song <yousonghou@yahoo.com>
2019-05-15 21:06:20 +0000

    add more icon

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,10 +4,11 @@ class Home extends Component {
     render(){
         return(
             <div className="home">
+                <p className="home__gen">1,000 GEN</p>
                 <div className="home__container">
-                    <h1>Title</h1>
+                    <h1 className="home__title">Title</h1>
                     <p className="home__description">Description</p>
-                    <p className="home__link">Link</p>
+                    <p className="home__link">Link -> <i class="far fa-sticky-note"></i></p>
                     <div className="home__group">
                         <i class="home__groupButton1 far fa-arrow-alt-circle-up"></i>
                         <i class="home__groupButton1 far fa-arrow-alt-circle-down"></i>
