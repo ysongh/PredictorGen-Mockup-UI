188e50e945a1fff2b0289cb24c3d504232286030
Song <yousonghou@yahoo.com>
2019-05-15 20:34:32 +0000

    add first container

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,7 +3,18 @@ import React, { Component } from 'react';
 class Home extends Component {
     render(){
         return(
-            <h1>title</h1>
+            <div className="home">
+                <div className="home__container">
+                    <h1>Title</h1>
+                    <p className="home__description">Description</p>
+                    <p className="home__link">Link</p>
+                    <div className="home__group">
+                        <button className="home__groupButton1">Yes</button>
+                        <button className="home__groupButton2">No</button>
+                    </div>
+                </div>
+            </div>
+            
         );
     }
 }
